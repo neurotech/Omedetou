75773a891cdc0e619715ab4a72e671928f51701d
Tim Douglas <neurotech@gmail.com>
2022-10-10 12:34:40 +1100

    Make it so.

---
+++ Omedetou.lua
@@ -0,0 +1,68 @@
+OMEDETOU_ACTIVE = false
+SLASH_OMEDETOU_TOGGLE1 = "/garts"
+
+local OmedetouCooldown = 0
+local OmedetouChatListener = CreateFrame("Frame")
+
+local OmedetouMessageTemplates = {
+  [1] = "garts",
+  [2] = "grats!",
+  [3] = "gratz",
+  [4] = "Congratulations on your achievement!!!!"
+}
+
+
+local function startswith(String, Start)
+  return string.sub(String, 1, string.len(Start)) == Start
+end
+
+local function GetChatMessage()
+  local randIndex = math.random(1, #OmedetouMessageTemplates)
+  return OmedetouMessageTemplates[randIndex]
+end
+
+local function ToggleOmedetou()
+  if OMEDETOU_ACTIVE then
+    OmedetouChatListener:RegisterEvent("CHAT_MSG_GUILD")
+    OmedetouChatListener:RegisterEvent("CHAT_MSG_GUILD_ACHIEVEMENT")
+
+    OmedetouChatListener:SetScript("OnEvent", function(self, event, ...)
+      local currentPlayer = UnitName("player")
+      local message = select(1, ...)
+      local sender = select(2, ...)
+      local playerName = sender:gsub("%-.+", "")
+      local isSelf = playerName == currentPlayer
+
+      -- DEBUG:
+      -- isSelf = false
+
+      local isAchievementText = startswith(tostring(message), "has earned the achievement ") or
+          startswith(message, "[Attune] |cff")
+
+      if isAchievementText then
+        if not isSelf then
+          if OmedetouCooldown == 0 then
+            OmedetouCooldown = 5
+            local delay = math.random(1, 2)
+
+            C_Timer.After(delay, function()
+              SendChatMessage(GetChatMessage(), "WHISPER", nil, playerName)
+            end)
+
+            C_Timer.After(OmedetouCooldown, function()
+              OmedetouCooldown = 0
+            end)
+          end
+        end
+      end
+    end)
+  else
+    OmedetouChatListener:UnregisterEvent("CHAT_MSG_GUILD")
+    OmedetouChatListener:UnregisterEvent("CHAT_MSG_GUILD_ACHIEVEMENT")
+  end
+end
+
+SlashCmdList["OMEDETOU_TOGGLE"] = function(msg, editBox)
+  OMEDETOU_ACTIVE = not OMEDETOU_ACTIVE
+  ToggleOmedetou()
+end
---
+++ dev.sh
@@ -0,0 +1,17 @@
+echo "Building Omedetou and installing to WoW directory."
+
+touch Omedetou.toc.tmp
+
+cat Omedetou.toc > Omedetou.toc.tmp
+
+sed -i "s/@project-version@/$(git describe --abbrev=0)/g" Omedetou.toc.tmp
+
+mkdir -p /h/games/World\ of\ Warcraft/_classic_/Interface/AddOns/Omedetou/
+
+cp *.lua *.ogg /h/games/World\ of\ Warcraft/_classic_/Interface/AddOns/Omedetou/
+
+cp Omedetou.toc.tmp /h/games/World\ of\ Warcraft/_classic_/Interface/AddOns/Omedetou/Omedetou.toc
+
+rm Omedetou.toc.tmp
+
+echo "Complete."
